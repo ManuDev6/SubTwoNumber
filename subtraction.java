1be2162f9421c2b1fb1619851c60ebdd0cfed304
ManuDev <manudevbro@gmail.com>
2026-07-26 23:51:58 +0200

    Update subtraction.java
    Replaced scanner.nextInt() with Integer.valueOf() for input handling.

--- subtraction.java
+++ subtraction.java
@@ -15,12 +15,12 @@ public class subtraction {
         // First number
 
         System.out.println("Enter first number: ");
-        int num1 = scanner.nextInt();
+        int num1 = Integer.valueOf(scanner.nextLine());
 
         // Second number
 
         System.out.println("Enter second number: ");
-        int num2 = scanner.nextInt();
+        int num2 = Integer.valueOf(scanner.nextLine());
 
         // Result
         
